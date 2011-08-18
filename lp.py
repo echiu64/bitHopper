136ff6578334538950c566721ffe0564ca402a72
Edwin <edwin@supersixfour.com>
2011-08-18 00:00:14 -0400

    Reset scheduler when new block found

--- lp.py
+++ lp.py
@@ -62,6 +62,7 @@ class LongPoll():
             self.set_owner(server)
             old_shares = self.bitHopper.pool.servers[server]['shares']
             self.bitHopper.pool.servers[server]['shares'] = 0
+	    self.bitHopper.scheduler.reset()
             self.bitHopper.select_best_server()
             if '_defer' not in self.blocks[block]:
                 self.blocks[block]['_defer'] = defer.Deferred()
